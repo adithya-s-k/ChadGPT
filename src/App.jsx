58242f6652482cba0beb0c9c2c82831993369831
Adithya S K <adithyaskolavi@gmail.com>
2023-01-23 22:02:39 +0530

    added text based APIS - Adithya S K

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Image from './pages/ImageGenerator';
+import Select from './pages/Selection';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/image" element={<Image />} />
+      <Route path="/select" element={<Select />} />
     </Routes>
   );
 }
